7f867e008f8f4fd8ecbc3439ae650322a2767412
烨煊桑w <m451577581@live.com>
2020-04-07 22:59:06 +0800

    添加项目文件。

---
+++ OCDataConverter/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace OCDataConverter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pathLabel = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.HorizontalScrollbar = true;
+            this.listBox1.ItemHeight = 17;
+            this.listBox1.Items.AddRange(new object[] {
+            "Waiting..."});
+            this.listBox1.Location = new System.Drawing.Point(14, 34);
+            this.listBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(546, 497);
+            this.listBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(412, 568);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(148, 41);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "convert";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(14, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 17);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Log";
+            // 
+            // pathLabel
+            // 
+            this.pathLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pathLabel.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.pathLabel.Location = new System.Drawing.Point(13, 539);
+            this.pathLabel.Name = "pathLabel";
+            this.pathLabel.Size = new System.Drawing.Size(547, 23);
+            this.pathLabel.TabIndex = 3;
+            this.pathLabel.Text = "path";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 569);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 40);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "selectPath";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(136, 569);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 40);
+            this.button3.TabIndex = 5;
+            this.button3.Text = "readTest";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(260, 592);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 17);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "record count: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(577, 620);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pathLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.listBox1);
+            this.Cursor = System.Windows.Forms.Cursors.Default;
+            this.Font = new System.Drawing.Font("微软雅黑", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OCDataConvert";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label pathLabel;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ OCDataConverter/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Windows.Forms;
+using System.IO;
+using MySql.Data.MySqlClient;
+
+
+namespace OCDataConverter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string query, RDBServer, RDBPort, RDBUser, RDBPassword, RDBDatabase;
+
+            RDBServer = "Your Host";
+            RDBPort = "Your Host Port";
+            RDBUser = "Your Username";
+            RDBPassword = "Your Password";
+            RDBDatabase = "Your Database";
+
+            int recordcount = 0;
+            label2.Text = "record count: " + recordcount.ToString();
+            listBox1.Items.Clear();
+            listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() + "] Working.");
+            listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+            button1.Enabled = false;
+
+            MySqlConnection RemoteDB = new MySqlConnection("Host=" + RDBServer + ";port=" + RDBPort +
+            ";User ID=" + RDBUser + ";password=" + RDBPassword + ";database=" + RDBDatabase + "");
+
+            listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() + "] Connecting to OC remote database...");
+            listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+            try
+            {
+                RemoteDB.Open();
+                listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() + "] Successfully connected to OC remote database.");
+                listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                DirectoryInfo OCUserPath = new DirectoryInfo(pathLabel.Text);
+                foreach (FileInfo NextFile in OCUserPath.GetFiles())
+                {
+                    listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() + "] Reading File:" + NextFile.Name);
+                    listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                    string UID = INIHelper.INIHelper.Read("record", "uid", "notlinked", pathLabel.Text + "\\" + NextFile.Name);
+                    string MainMode = INIHelper.INIHelper.Read("record", "mainmode", "0", pathLabel.Text + "\\" + NextFile.Name);
+                    string UserName = INIHelper.INIHelper.Read("record", "username", "notlinked", pathLabel.Text + "\\" + NextFile.Name);
+                    string QQ = NextFile.Name.Substring(0, NextFile.Name.Length - 4);
+                    listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() +
+                        "] UID:" + UID + "  Username:" + UserName + "  QQ:" + QQ + "  MainMode:" + MainMode);
+                    listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                    listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() + "] Pushing User " + UID + " ...");
+                    listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                    query = "INSERT INTO info (uid, qq, mainmode) VALUES (" + UID + ", " + QQ + ", " + MainMode + ")";
+                    MySqlCommand CMD = new MySqlCommand(query, RemoteDB);
+                    CMD.ExecuteNonQuery();
+                    listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() + "] Done..");
+                    listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                    recordcount = recordcount + 1;
+                    label2.Text = "record count: " + recordcount.ToString();
+                    listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() + "] " + recordcount.ToString() + " records have been exported.");
+                    listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                }
+                RemoteDB.Close();
+                listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() + "] Your request has successfully submitted.");
+                listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() + "] The database connection has closed.");
+                listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                MessageBox.Show("Your request has successfully submitted.", "successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                button1.Enabled = true;
+            }
+            catch (MySqlException err)
+            {
+                RemoteDB.Close();
+                listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() + "] The database connection has closed.");
+                listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() + "] An error has occurred during your session.");
+                listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() + "] Details: " + err.Message);
+                listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                MessageBox.Show("An error has occurred during your session.\r\nSee the log for more details.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                button1.Enabled = true;
+            }
+            catch (Exception err)
+            {
+                RemoteDB.Close();
+                listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() + "] The database connection has closed.");
+                listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() + "] An error has occurred during your session.");
+                listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() + "] Details: " + err.Message);
+                listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                MessageBox.Show("An error has occurred during your session.\r\nSee the log for more details.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                button1.Enabled = true;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dilog = new FolderBrowserDialog();
+            dilog.Description = "Choose a folder.";
+            if (dilog.ShowDialog() == DialogResult.OK || dilog.ShowDialog() == DialogResult.Yes)
+            {
+                pathLabel.Text = dilog.SelectedPath;
+                DirectoryInfo OCUserPath = new DirectoryInfo(pathLabel.Text);
+                listBox1.Items.Clear();
+                listBox1.Items.Add("List of this folder:");
+                listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                listBox1.Items.Add("*********************");
+                listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                foreach (FileInfo NextFile in OCUserPath.GetFiles())
+                {
+                    listBox1.Items.Add(NextFile.Name);
+                }
+                listBox1.Items.Add(" ");
+                listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                listBox1.Items.Add("Subfolder of this folder:");
+                listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                listBox1.Items.Add("****************************");
+                listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                foreach (DirectoryInfo NextFolder in OCUserPath.GetDirectories())
+                {
+                    listBox1.Items.Add(NextFolder.Name);
+                    listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                }
+                listBox1.Items.Add(" ");
+                listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() + "] If this is OK then start convert.");
+                listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            int recordcount = 0;
+            label2.Text = "record count: " + recordcount.ToString();
+            listBox1.Items.Clear();
+            listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() + "] Working.");
+            listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+            button3.Enabled = false;
+            try
+            {
+                DirectoryInfo OCUserPath = new DirectoryInfo(pathLabel.Text);
+                foreach (FileInfo NextFile in OCUserPath.GetFiles())
+                {
+                    listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() + "] Reading File:" + NextFile.Name);
+                    listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                    string UID = INIHelper.INIHelper.Read("record", "uid", "notlinked", pathLabel.Text + "\\" + NextFile.Name);
+                    string MainMode = INIHelper.INIHelper.Read("record", "mainmode", "0", pathLabel.Text + "\\" + NextFile.Name);
+                    string UserName = INIHelper.INIHelper.Read("record", "username", "notlinked", pathLabel.Text + "\\" + NextFile.Name);
+                    string QQ = NextFile.Name.Substring(0, NextFile.Name.Length - 4);
+                    listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() +
+                        "] UID:" + UID + "  Username:" + UserName + "  QQ:" + QQ + "  MainMode:" + MainMode);
+                    listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                    recordcount = recordcount + 1;
+                    label2.Text = "record count: " + recordcount.ToString();
+                    listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() + "] " + recordcount.ToString() + " records have been exported.");
+                    listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                }
+                listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() + "] Your request has successfully submitted.");
+                listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                MessageBox.Show("Your request has successfully submitted.", "successful", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                button3.Enabled = true;
+            }
+            catch (MySqlException err)
+            {
+                listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() + "] The database connection has closed.");
+                listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() + "] An error has occurred during your session.");
+                listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() + "] Details: " + err.Message);
+                listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                MessageBox.Show("An error has occurred during your session.\r\nSee the log for more details.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                button1.Enabled = true;
+            }
+            catch (Exception err)
+            {
+                listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() + "] The database connection has closed.");
+                listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() + "] An error has occurred during your session.");
+                listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                listBox1.Items.Add("[" + DateTime.Now.ToLongTimeString().ToString() + "] Details: " + err.Message);
+                listBox1.TopIndex = listBox1.Items.Count - (listBox1.Height / listBox1.ItemHeight);
+                MessageBox.Show("An error has occurred during your session.\r\nSee the log for more details.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                button1.Enabled = true;
+            }
+        }
+    }
+}
